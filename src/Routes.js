c865fe8cb2bae38291fe17a0b6ab61b5ef5c4a26
anvun <81310185+anvun@users.noreply.github.com>
2021-05-20 17:29:29 +0300

    Add files via upload

---
+++ src/Routes.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import AllRecipes from "./pages/AllRecipes";
+import Favourites from "./pages/Favourites";
+import MyRecipes from "./pages/MyRecipes";
+import Login from "./components/Login";
+
+
+export const publicRoutes = [
+    {
+        path: '/all-recipes',
+        Component: AllRecipes,
+        title: "Все рецепты", 
+        icon: "fas fa-hamburger"      
+    }
+]
+
+export const privateRoutes = [
+    {
+        path: '/my-recipes',
+        Component: MyRecipes,
+        title: "Мои рецепты",
+        icon: "fas fa-clipboard-list"
+    },
+    {
+        path: '/favourites',
+        Component: Favourites,
+        title: "Избранное",
+        icon: "fa fa-bookmark"
+    }
+]
+
+export const accounRoutes = [
+    {
+        path: '/login',
+        Component: Login,
+        icon: "fas fa-user"
+    }
+]
