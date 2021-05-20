9b387b332b755263a6eb64d6b3db3bf4af68ed98
anvun <81310185+anvun@users.noreply.github.com>
2021-05-20 13:31:37 +0300

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {NavLink} from 'react-router-dom';
 
-function Sidebar() {
-  return (
-    <aside className="sidebar">
-        <h3>Logo</h3>
-        <ul className="sidebar__list">
-          <Link to='/all-recipes'>
-            <li>Все рецепты</li>
-          </Link>
-          <Link to='/my-recipes'>
-            <li>Мои рецепты</li>
-          </Link>
-          <Link to='/favourites'>
-            <li>Избранное</li>
-          </Link>
-        </ul>
-    </aside>
-  );
+function Sidebar(){
+  return ( 
+    <>
+      <nav className="sidebar">
+        <div className="sidebar-container">
+          <ul className="sidebar__list">
+            <li className="nav-item">
+              <NavLink exact to="/" className="nav-logo">
+                <img src="../../logo192.png"/>
+                <span>На здоровье</span>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/all-recipes" activeClassName="active" className="nav-links">
+                <i class="fas fa-hamburger"></i>
+                Все рецепты
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/my-recipes" activeClassName="active" className="nav-links">
+                <i class="fas fa-clipboard-list"></i>
+                Мои рецепты
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/favourites" activeClassName="active" className="nav-links">
+                <i class="fa fa-bookmark" aria-hidden="true"></i>
+                Избранное
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Sidebar;
