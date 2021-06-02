51d4c62c7645e0da86628da3437e9f335ba04a09
anvun <81310185+anvun@users.noreply.github.com>
2021-06-02 19:02:13 +0300

    Add files via upload

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -6,41 +6,45 @@ class RecipeCard extends Component {
         const Recipe = this.props.recipe
         return(
             <div className='recipecard'>
+                <div className="card-image">
+                    <img className='img'
+                        src="https://static.1000.menu/img/content-v2/69/74/51856/pankeiki-na-moloke-s-yaicom-sodoi-i-limonnoi-kislotoi_1614588187_8_max.jpg" 
+                        alt="logo"/>
+                </div>
                 <div className='recipecard-container'>
-                    <img
-                        className='img'
-                        src="../../logo192.png" 
-                        alt="logo"
-                    />
-                <div className='title'>
-                    <h1>
-                        {Recipe.title}
-                    </h1>
-                </div>
-                <div className='category'>
-                    <h2>
-                        {Recipe.category}
-                    </h2>
-                </div>
-                <div className='describing'>
-                    <h3>
-                        {Recipe.describing}
-                    </h3>
-                </div>
-                <button className='like' onClick={handleClick}>
-                    <i class="fas fa-heart"></i>
-                </button>
+                    <div className="card-content">
+                        <div className='title'>
+                            <h1>
+                                {Recipe.title}
+                            </h1>
+                        </div>
+                        <div className='category'>
+                            <h2>
+                                {Recipe.category}
+                            </h2>
+                        </div>
+                        <div className='describing'>
+                            <h3>
+                                {Recipe.describing}
+                            </h3>
+                        </div>
 
-                <div className='recipe-card_extend'/* у згорнутому варіанті даного блоку не має бути видно */>
-                    <div className='recipe-card_ingredients'>
-                        {Recipe.ingredients}
+                        <div className='recipe-card_extend'/* у згорнутому варіанті даного блоку не має бути видно */>
+                            <div className='recipe-card_ingredients'>
+                                {Recipe.ingredients}
+                            </div>
+                            <div className='recipe-card_cooking'>
+                                {Recipe.cooking}
+                            </div>
+                        </div>
                     </div>
-                    <div className='recipe-card_cooking'>
-                        {Recipe.cooking}
+                    <div className="card-like">
+                        <button className='like' onClick={handleClick}>
+                            <i class="fas fa-heart"></i>
+                        </button>
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
--- src/components/RecipeCardList.js
+++ src/components/RecipeCardList.js
@@ -10,7 +10,7 @@ import RecipeCard from "./RecipeCard";
             </li>
         )
         return(
-            <ul>
+            <ul className="cards">
                 {recipeElements}
             </ul>
         )
